40eb0b3a4c15e9ba4ec056b6d51445de418a6d4d
Daniel Bowden <dj.bowden7@gmail.com>
2022-10-24 19:08:51 -0400

    removing dependncies

--- src/main/kotlin/com/catalogsync/adapters/DaprEventBus.kt
+++ src/main/kotlin/com/catalogsync/adapters/DaprEventBus.kt
@@ -1,18 +1,18 @@
-package com.catalogsync.adapters
-
-import com.catalogsync.ports.EventBusPort
-import io.dapr.client.DaprClient
-import javax.enterprise.context.ApplicationScoped
-import javax.inject.Inject
-
-@ApplicationScoped
-class DaprEventBus constructor(@Inject val daprClient: DaprClient): EventBusPort {
-    val PUB_SUB_NAME = "redis_pub_sub"
-    override fun <T> sendMessage(payload: T, topic: String) {
-        daprClient.publishEvent(
-            PUB_SUB_NAME,
-            topic,
-            payload
-        ).block()
-    }
-}
+//package com.catalogsync.adapters
+//
+//import com.catalogsync.ports.EventBusPort
+//import io.dapr.client.DaprClient
+//import javax.enterprise.context.ApplicationScoped
+//import javax.inject.Inject
+//
+//@ApplicationScoped
+//class DaprEventBus constructor(@Inject val daprClient: DaprClient): EventBusPort {
+//    val PUB_SUB_NAME = "redis_pub_sub"
+//    override fun <T> sendMessage(payload: T, topic: String) {
+//        daprClient.publishEvent(
+//            PUB_SUB_NAME,
+//            topic,
+//            payload
+//        ).block()
+//    }
+//}
