639e7773309b7418474a3565ba4228a579fbc3c0
Daniel Bowden <dj.bowden7@gmail.com>
2022-10-25 23:15:03 -0400

    dapr v2

--- src/main/kotlin/com/catalogsync/adapters/DaprProducer.kt
+++ src/main/kotlin/com/catalogsync/adapters/DaprProducer.kt
@@ -1,14 +1,14 @@
-package com.catalogsync.adapters
-
-import io.dapr.client.DaprClient
-import io.dapr.client.DaprClientBuilder
-import javax.enterprise.context.ApplicationScoped
-import javax.enterprise.inject.Produces
-
-@ApplicationScoped
-class DaprProducer {
-    @Produces
-    fun getDaprClient(): DaprClient = DaprClientBuilder().build()
-
-}
-
+//package com.catalogsync.adapters
+//
+//import io.dapr.client.DaprClient
+//import io.dapr.client.DaprClientBuilder
+//import javax.enterprise.context.ApplicationScoped
+//import javax.enterprise.inject.Produces
+//
+//@ApplicationScoped
+//class DaprProducer {
+//    @Produces
+//    fun getDaprClient(): DaprClient = DaprClientBuilder().build()
+//
+//}
+//
